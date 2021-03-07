939ab55d0b950272fa16344fe85ad8f65e03daa7
Miikka Sainio <miikka.sainio@gmail.com>
2021-03-07 10:44:37 +0200

    brakes comment

--- gym/envs/box2d/car_dynamics.py
+++ gym/envs/box2d/car_dynamics.py
@@ -130,7 +130,7 @@ class Car:
 
         Args:
             b (0..1): Degree to which the brakes are applied. More than 0.9 blocks the wheels to zero rotation"""
-        for w in self.wheels[2:4]:
+        for w in self.wheels[2:4]: #brake only rear wheels
             w.brake = b
 
     def steer(self, s):
